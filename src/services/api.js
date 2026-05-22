f8c038cd90edf6da3fc9aa4980e92ef1bedf8bac
NILESH KUMAR <140897234+rnileshk@users.noreply.github.com>
2026-05-22 21:01:14 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://trongon.onrender.com/api",
 });
 
 api.interceptors.request.use(
